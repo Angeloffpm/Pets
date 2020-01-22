e5510f859cbe5a95423f85f337e327ac95d8da68
Pierce Michael Angeloff <angeloffpm@s.dcsdk12.org>
2020-01-22 08:19:05 -0700

    Added testing class

---
+++ Pets.java
@@ -0,0 +1,12 @@
+public class Pets {
+
+    public static void main(String[] args) {
+        
+        Dog louie = new Dog(false, true, false);
+        Cat kenya = new Cat(false, false, true);
+
+        PetOwner pierce = new PetOwner(louie, kenya);
+
+    }
+
+}
