bed65bbc332130cfd550f1249a743c3135726772
Tayler Allen Stegman <stegmanta@s.dcsdk12.org>
2020-01-22 08:18:30 -0700

    Started editing cat

--- Cat.java
+++ Cat.java
@@ -4,4 +4,42 @@ public class Cat {
     private boolean walked;
     private boolean petted;
 
+    //constructor
+
+    public Cat(boolean fed, boolean walked, boolean petted){
+        this.fed = fed;
+        this.walked = walked;
+        this.petted = petted;
+    }
+
+    //functions
+    public boolean HappyCat(boolean fed,boolean walked,boolean petted){
+        if(fed && !walked && !petted){
+            return true;
+        }
+        return false;
+    }
+
+
+    public String hissOrMeow(boolean HappyCat){
+        if(HappyCat){
+            return "Meow";
+        }
+        return "HISSS";
+    }
+
+     // Setters (Pet Owner interactions)
+     public void feed() {
+        this.fed = true;
+    }
+
+    public void walk() {
+        this.walked = true;
+    }
+    
+    public void pet() {
+        this.petted = true;
+    }
+
+
 }
