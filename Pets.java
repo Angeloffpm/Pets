3f01bfcf1fcbba97e583ee12acc426b2cfa968cf
Pierce Michael Angeloff <angeloffpm@s.dcsdk12.org>
2020-01-27 08:23:22 -0700

    edited pets

--- Pets.java
+++ Pets.java
@@ -4,13 +4,19 @@ public class Pets {
 
         TalkingCat garfield = new TalkingCat("Garfield");
         System.out.println(garfield.speak());
-        System.out.println(garfield.speak());
-        System.out.println(garfield.speak());
         garfield.feed();
-        System.out.println(garfield.speak());
         System.out.println(garfield.speak());
 
         Dog odie = new Dog("Odie");
+
+        Horse horse = new Horse("Seabiscuit");
+        System.out.println(horse.isHappy());
+        horse.brush();
+        horse.ride();
+        horse.feed();
+        horse.walk();
+        horse.pet();
+        System.out.println(horse.isHappy());
 
     }
 
